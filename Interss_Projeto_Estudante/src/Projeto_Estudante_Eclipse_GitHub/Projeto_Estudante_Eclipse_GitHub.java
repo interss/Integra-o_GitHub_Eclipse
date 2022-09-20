0bbde29357cbc61b18c2213cfcf5c19666b851a9
Conta <Conta@DESKTOP-FNBMR9S>
2022-09-20 16:04:52 -0300

    Testadno envio de mudanças por outro caminho dentro do Eclipse

--- Interss_Projeto_Estudante/src/Projeto_Estudante_Eclipse_GitHub/Projeto_Estudante_Eclipse_GitHub.java
+++ Interss_Projeto_Estudante/src/Projeto_Estudante_Eclipse_GitHub/Projeto_Estudante_Eclipse_GitHub.java
@@ -14,6 +14,9 @@ public class Projeto_Estudante_Eclipse_GitHub {
 		System.out.println("Hello Git, até que enfim hein kkkk");
 		System.out.println("Será que agora deu certo? Mas não apareceu o package, onde foi o erro agora hein? Vamos estudar pra ver kkkk");
         System.out.println("Tudo indica que a integração foi feita com sucesso! Vejamos agora se ao enviar o COMMIT aparecerá no GitHub");
+        System.out.println("Fazendo teste de envio de COMMIT por outro caminho no Eclipse");
+        System.out.println("Tudo indica que vai dar certo! Brother, isso é muito legal demaiiiis kkkk");
+        
 	}
 
 }
