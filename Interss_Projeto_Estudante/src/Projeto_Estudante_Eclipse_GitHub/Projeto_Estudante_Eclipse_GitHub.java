506302d3ddfbd233ff15663fc36aa87ab625f706
Conta <Conta@DESKTOP-FNBMR9S>
2022-09-20 15:53:45 -0300

    Testando COMMIT do Eclipse para o GitHub

--- Interss_Projeto_Estudante/src/Projeto_Estudante_Eclipse_GitHub/Projeto_Estudante_Eclipse_GitHub.java
+++ Interss_Projeto_Estudante/src/Projeto_Estudante_Eclipse_GitHub/Projeto_Estudante_Eclipse_GitHub.java
@@ -13,7 +13,7 @@ public class Projeto_Estudante_Eclipse_GitHub {
 	public static void main(String[] args) {
 		System.out.println("Hello Git, até que enfim hein kkkk");
 		System.out.println("Será que agora deu certo? Mas não apareceu o package, onde foi o erro agora hein? Vamos estudar pra ver kkkk");
-
+        System.out.println("Tudo indica que a integração foi feita com sucesso! Vejamos agora se ao enviar o COMMIT aparecerá no GitHub");
 	}
 
 }
