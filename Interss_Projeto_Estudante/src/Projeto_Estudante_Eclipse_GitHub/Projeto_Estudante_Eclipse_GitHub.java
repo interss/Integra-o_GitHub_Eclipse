bcc54851ac9552a5e16049f6590b188798432ba5
interss <113519414+interss@users.noreply.github.com>
2022-09-20 15:28:50 -0300

    Carregado projeto estudante integração Git/Eclipse

---
+++ Interss_Projeto_Estudante/src/Projeto_Estudante_Eclipse_GitHub/Projeto_Estudante_Eclipse_GitHub.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Projeto_Estudante_Eclipse_GitHub;
+
+/**
+ * @author Conta
+ *Entendendo como funciona a integração do Eclipse com o GitHub
+ */
+public class Projeto_Estudante_Eclipse_GitHub {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git, até que enfim hein kkkk");
+
+	}
+
+}
